f110f3fad49313d54a04c5bf1049e539089e00d2
x30044283 <xsmchengdu@163.com>
2024-05-22 00:52:19 +0800

    [sharding5]通过sharding-jdbc查询

---
+++ sharding5-demo/src/main/java/com/xusm/config/ShardingDatasource.java
@@ -0,0 +1,19 @@
+//package com.xusm.config;
+//
+//import org.apache.shardingsphere.driver.api.yaml.YamlShardingSphereDataSourceFactory;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//
+//import javax.sql.DataSource;
+//
+//@Configuration
+//public class ShardingDatasource {
+//
+//    @Bean
+//    public DataSource dataSource() {
+//        File yamlFile = // 指定 YAML 文件路径
+//        DataSource dataSource = YamlShardingSphereDataSourceFactory.createDataSource(yamlFile);
+//        return dataSource;
+//    }
+//
+//}
