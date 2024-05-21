ad7c4050a23e35a99f73ef3f301398409c368bc4
x30044283 <xsmchengdu@163.com>
2024-05-21 11:05:03 +0800

    [sharding]初始化mybatis-plus查询

---
+++ sharding-demo/src/main/java/com/xusm/entity/StoreInfo.java
@@ -0,0 +1,14 @@
+package com.xusm.entity;
+
+import lombok.Data;
+
+@Data
+public class StoreInfo {
+    private Integer id;
+
+    private String storeName;
+
+    private int reputation;
+
+    private String regionCode;
+}
---
+++ sharding-demo/src/main/java/com/xusm/mapper/StoreMapper.java
@@ -0,0 +1,9 @@
+package com.xusm.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.xusm.entity.StoreInfo;
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface StoreMapper extends BaseMapper<StoreInfo> {
+}
