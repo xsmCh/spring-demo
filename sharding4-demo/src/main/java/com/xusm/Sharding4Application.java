50e027ed76a43e851eac5011cb65dd8e2cf00321
x30044283 <xsmchengdu@163.com>
2024-05-21 22:21:04 +0800

    [sharding5]创建sharding5-demo

--- sharding4-demo/src/main/java/com/xusm/Sharding4Application.java
+++ sharding4-demo/src/main/java/com/xusm/Sharding4Application.java
@@ -4,8 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ShardingApplication {
+public class Sharding4Application {
     public static void main(String[] args) {
-        SpringApplication.run(ShardingApplication.class, args);
+        SpringApplication.run(Sharding4Application.class, args);
     }
 }
