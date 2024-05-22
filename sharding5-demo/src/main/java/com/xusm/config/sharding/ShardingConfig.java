7d94572d958042d53c68c9e8d5e7bfee1f08551f
x30044283 <xsmchengdu@163.com>
2024-05-22 11:21:07 +0800

    [sharding5]强制初始化数据源

--- sharding5-demo/src/main/java/com/xusm/config/sharding/ShardingConfig.java
+++ sharding5-demo/src/main/java/com/xusm/config/sharding/ShardingConfig.java
@@ -6,14 +6,28 @@ import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.context.annotation.Configuration;
 
+import javax.annotation.Resource;
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.SQLException;
+
 @Slf4j
 @Configuration
 public class ShardingConfig implements ApplicationRunner {
     @Value("${sharding.file}")
     private String shardingFile;
 
+    @Resource
+    private DataSource dataSource;
+
     @Override
-    public void run(ApplicationArguments args) {
+    public void run(ApplicationArguments args) throws SQLException {
+        // 输出启动的sharding file
         log.info("sharding file: {}", shardingFile);
+        // 强制初始化数据源
+        try (Connection connection = dataSource.getConnection()) {
+            // This will ensure that the HikariDataSource is started at application startup
+            log.info("HikariDataSource has been initialized and a connection has been acquired.");
+        }
     }
 }
