cfa001305b6e6b06efe909f70c5da50694015ddb
x30044283 <xsmchengdu@163.com>
2024-08-13 14:09:57 +0800

    [sharding5-nacos]使用数据源创建sharding

--- sharding5-demo/src/main/java/com/xusm/config/dynamic/DynamicDatasourceConfig.java
+++ sharding5-demo/src/main/java/com/xusm/config/dynamic/DynamicDatasourceConfig.java
@@ -1,6 +1,8 @@
 package com.xusm.config.dynamic;
 
 import com.xusm.component.DynamicRoutingDataSource;
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.shardingsphere.driver.api.yaml.YamlShardingSphereDataSourceFactory;
 import org.apache.shardingsphere.driver.jdbc.core.driver.ShardingSphereURLManager;
@@ -38,29 +40,68 @@ public class DynamicDatasourceConfig {
     }
 
     public DataSource database1() throws SQLException, IOException {
+        HikariDataSource m0 = hikariDataSource("1-m0", "jdbc:mysql://localhost:3306/store_db?useUnicode=true", "root", "root");
+        Map<String, DataSource> dataSourceMap = new HashMap<>();
+        dataSourceMap.put("m0", m0);
+
         String url = "jdbc:shardingsphere:classpath:sharding-jdbc-single.yaml";
         String urlPrefix = "jdbc:shardingsphere:";
         log.info("start init sharding jdbc");
-        DataSource dataSource = YamlShardingSphereDataSourceFactory.createDataSource(ShardingSphereURLManager.getContent(url, urlPrefix));
+        DataSource dataSource = YamlShardingSphereDataSourceFactory.createDataSource(dataSourceMap, ShardingSphereURLManager.getContent(url, urlPrefix));
         log.info("end init sharding jdbc");
         return dataSource;
     }
 
     public DataSource database2() throws SQLException, IOException {
+        HikariDataSource m0 = hikariDataSource("2-m0", "jdbc:mysql://localhost:3306/store_db?useUnicode=true", "root", "root");
+        HikariDataSource m1 = hikariDataSource("2-m1", "jdbc:mysql://localhost:3306/product_db_1?useUnicode=true", "root", "root");
+        Map<String, DataSource> dataSourceMap = new HashMap<>();
+        dataSourceMap.put("m0", m0);
+        dataSourceMap.put("m1", m1);
+
         String url = "jdbc:shardingsphere:classpath:sharding-jdbc-m1.yaml";
         String urlPrefix = "jdbc:shardingsphere:";
         log.info("start init sharding jdbc");
-        DataSource dataSource = YamlShardingSphereDataSourceFactory.createDataSource(ShardingSphereURLManager.getContent(url, urlPrefix));
+        DataSource dataSource = YamlShardingSphereDataSourceFactory.createDataSource(dataSourceMap, ShardingSphereURLManager.getContent(url, urlPrefix));
         log.info("end init sharding jdbc");
         return dataSource;
     }
 
     public DataSource database3() throws SQLException, IOException {
+        HikariDataSource m0 = hikariDataSource("3-m0", "jdbc:mysql://localhost:3306/store_db?useUnicode=true", "root", "root");
+        HikariDataSource m1 = hikariDataSource("3-m1", "jdbc:mysql://localhost:3306/product_db_1?useUnicode=true", "root", "root");
+        HikariDataSource m3 = hikariDataSource("3-m2", "jdbc:mysql://localhost:3306/product_db_2?useUnicode=true", "root", "root");
+        Map<String, DataSource> dataSourceMap = new HashMap<>();
+        dataSourceMap.put("m0", m0);
+        dataSourceMap.put("m1", m1);
+        dataSourceMap.put("m3", m3);
+
         String url = "jdbc:shardingsphere:classpath:sharding-jdbc.yaml";
         String urlPrefix = "jdbc:shardingsphere:";
         log.info("start init sharding jdbc");
-        DataSource dataSource = YamlShardingSphereDataSourceFactory.createDataSource(ShardingSphereURLManager.getContent(url, urlPrefix));
+        DataSource dataSource = YamlShardingSphereDataSourceFactory.createDataSource(dataSourceMap, ShardingSphereURLManager.getContent(url, urlPrefix));
         log.info("end init sharding jdbc");
         return dataSource;
+    }
+
+    private HikariDataSource hikariDataSource(String poolName, String url, String userName, String passwd) {
+        // 创建Hikari配置
+        HikariConfig config = new HikariConfig();
+
+        // 设置JDBC URL、用户名和密码
+        config.setJdbcUrl(url);
+        config.setUsername(userName);
+        config.setPassword(passwd);
+
+        // 配置Hikari连接池参数
+        config.setMaximumPoolSize(10);        // 设置最大连接数
+        config.setMinimumIdle(5);             // 设置最小空闲连接数
+        config.setIdleTimeout(30000);         // 设置连接空闲超时时间，单位毫秒
+        config.setConnectionTimeout(30000);   // 设置连接超时时间，单位毫秒
+        config.setMaxLifetime(1800000);       // 设置连接的最大生命周期，单位毫秒
+        config.setPoolName(poolName);     // 设置连接池的名字
+
+        // 创建HikariDataSource实例
+        return new HikariDataSource(config);
     }
 }
