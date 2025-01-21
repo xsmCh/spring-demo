883f40fff765ca0679ee038ef8674bbdee9560a9
x30044283 <xsmchengdu@163.com>
2025-01-21 21:51:02 +0800

    [selenium-demo]用jdk17跑通第一个页面（需要很多解释）

--- selenium-demo/src/test/java/com/xusm/selenium/GoogleSearchTest.java
+++ selenium-demo/src/test/java/com/xusm/selenium/GoogleSearchTest.java
@@ -31,13 +31,14 @@ public class GoogleSearchTest {
         ChromeOptions options = new ChromeOptions();
         options.addArguments("--headless");  // 启用无头模式
         options.addArguments("--disable-gpu"); // 禁用 GPU
+        options.addArguments("--disable-blink-features=AutomationControlled"); // 反添加爬虫 https://blog.csdn.net/qq_18946341/article/details/127428510
 
         // 创建 WebDriver 实例并返回
         return new ChromeDriver(options);
     }
 
     @Test
-    public void testGoogleSearch() {
+    public void testBSearch() {
         // 打开 Google 页面
         driver.get("https://www.bilibili.com");
 
@@ -54,6 +55,27 @@ public class GoogleSearchTest {
 
         // 等待页面加载，并验证页面标题是否包含关键词
         assertTrue(pageSource.contains("Selenium WebDriver"));
+    }
+
+    // 但是无头模式不行
+    @Test
+    public void testGoogleSearch() {
+        // 打开 Google 页面
+        driver.get("https://www.google.com");
+
+        // 查找搜索框并输入搜索内容
+        WebElement searchBox = driver.findElement(By.name("q"));
+        searchBox.sendKeys("Selenium WebDriver");
+
+        // 提交搜索
+        searchBox.submit();
+
+        // 获取当前页面的 HTML 源代码
+        String pageSource = driver.getPageSource();
+        log.info("Page is:{}{}", System.lineSeparator(), pageSource);
+
+        // 等待页面加载，并验证页面标题是否包含关键词
+        assertTrue(driver.getTitle().contains("Selenium WebDriver"));
     }
 
     @AfterEach
